8783c4c6ffbd11088d70252a25660ae65a3b0fd6
Christian Ramos <christivn.ramos@gmail.com>
2025-02-19 00:29:03 +0100

    update

--- webUI/routes/dashboard.py
+++ webUI/routes/dashboard.py
@@ -4,4 +4,7 @@ from server import app
 # Página principal (Dashboard)
 @app.route("/")
 def index():
-    return render_template('dashboard.html')
+    return render_template(
+        'dashboard.html',
+        aiAgent_uuid="123456789"
+    )
