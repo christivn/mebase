0c97cdf114a25fa098c8e5c0c2b1ff869a3ae6d2
Christian Ramos <christivn.ramos@gmail.com>
2025-02-19 00:52:24 +0100

    update

--- webUI/routes/logout.py
+++ webUI/routes/logout.py
@@ -1,6 +1,6 @@
 from flask import request, session, render_template, redirect
 from server import app
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
+@app.route("/logout")
+def logout():
+    return redirect("/login", code=302)
