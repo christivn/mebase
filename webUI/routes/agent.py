9bf283c0c5d66c7a685ea429507e58aafb63e152
Christian Ramos <christivn.ramos@gmail.com>
2025-02-20 23:59:41 +0100

    update

--- webUI/routes/agent.py
+++ webUI/routes/agent.py
@@ -1,6 +1,12 @@
 from flask import request, session, render_template, redirect
 from server import app
 
+@app.route("/create/agent")
+def create_agent():
+    return render_template(
+        'agent.html'
+    )
+
 @app.route("/agent/<agent_uuid>")
 def agent(agent_uuid):
     return render_template(
