3971f289b9a0f4067dfdc42fca43684319e9d7f0
Christian Ramos <christivn.ramos@gmail.com>
2025-02-27 16:16:55 +0100

    update

--- webUI/routes/chat.py
+++ webUI/routes/chat.py
@@ -1,4 +1,4 @@
-from flask import request, session, render_template, redirect
+from flask import request, session, render_template, redirect, jsonify
 from server import app
 
 @app.route("/")
@@ -7,6 +7,13 @@ def chat():
         'chat.html',
         modelDisplayName = "MeboxAI"
     )
+
+
+@app.route("/chat/create", methods = ['POST'])
+def create_chat():
+    if request.method == 'POST':
+        return jsonify({"message": "ok"}), 200
+
 
 @app.route("/<model>")
 def chat_model(model):
